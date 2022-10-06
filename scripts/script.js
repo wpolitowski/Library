56e2520cd99a6ea44fbdd581d457600e80b12710
wpolitowski <wojciechpolitowski@protonmail.ch>
2022-10-06 11:41:02 +0200

    Set up the project, create a popup window to add a new book

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+let myLibrary = [];
+// const addBookBtn = document.querySelector('.add-book');
+
+// addBookBtn.addEventListener('click', () => {
+    
+// })
+
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;  
+}
+
+function addBookToLibrary() {
+    
+}
+
+function displayBook() {
+
+}
