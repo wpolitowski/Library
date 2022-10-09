55a9f39c9cbe70d1bd30cae3be4e7f60d4da061b
wpolitowski <wojciechpolitowski@protonmail.ch>
2022-10-09 12:11:54 +0200

    Allow to add new books without refreshing the page after every form submission

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,51 @@
 let myLibrary = [];
-// const addBookBtn = document.querySelector('.add-book');
+// const addBookBtn = document.querySelector('button[type="submit"]');
+const addBookBtn = document.querySelector('form');
+const formFields = document.querySelectorAll('input, textarea');
 
-// addBookBtn.addEventListener('click', () => {
+
+addBookBtn.addEventListener('submit', (event) => {
+    event.preventDefault();
     
-// })
+    let validForm = true;
+    formFields.forEach( form => {
+        if(form.matches(':invalid')) validForm = false;
+    });
+
+    if (validForm) {
+        addBookToLibrary();
+        window.location = "#";
+        clearInputFields();
+    }
+    
+});
+
+function clearInputFields() {
+    formFields.forEach( form => {
+        if (form[type="checkbox"]) form.checked = false;
+        else form.value = "";
+    });
+}
 
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, comment, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.comment = comment;
     this.read = read;  
 }
 
 function addBookToLibrary() {
-    
+    const newBook = new Book(
+        formFields[0].value,
+        formFields[1].value,
+        formFields[2].value,
+        formFields[3].value,
+        formFields[4].checked
+    );
+
+    myLibrary.push(newBook);
 }
 
 function displayBook() {
